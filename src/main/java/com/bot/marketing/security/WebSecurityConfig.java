7d7d119da9578a2a6e71623f35a3fbec24cc8f4f
SProkopios <119333647+SProkopios@users.noreply.github.com>
2024-08-22 14:18:49 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/bot/marketing/security/WebSecurityConfig.java
+++ src/main/java/com/bot/marketing/security/WebSecurityConfig.java
@@ -15,8 +15,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class WebSecurityConfig {
 	
-	private static String username = System.getenv("user");
-	private static String password = System.getenv("password");
+	private static String username = System.getenv("USER");
+	private static String password = System.getenv("PASSWORD");
 
     
 	//Security for api users
