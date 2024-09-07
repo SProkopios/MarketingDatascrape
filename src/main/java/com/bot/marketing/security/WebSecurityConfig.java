c255c1fbdb5a531bd36fdf84f0a561c5dfbe54d5
SProkopios <s.koponen93@gmail.com>
2024-09-07 18:01:39 +0300

    adding CORS to restapi

--- src/main/java/com/bot/marketing/security/WebSecurityConfig.java
+++ src/main/java/com/bot/marketing/security/WebSecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -17,24 +20,27 @@ public class WebSecurityConfig {
 	
 	private static String username = System.getenv("username");
 	private static String password = System.getenv("password");
+	private static String address = System.getenv("address");
 
     
 	//Security for api users
 	@Bean
 	public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
-		
-		http
-			.securityMatcher("/api/**")
-			.authorizeHttpRequests((requests) -> requests
-				.anyRequest().authenticated()
-				)
-			.addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-			.csrf().disable();
-		
+		try {
+			http
+				.securityMatcher("/api/**")
+				.authorizeHttpRequests((requests) -> requests
+					.anyRequest().authenticated()
+						)
+				.addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.csrf().disable()
+				.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+		} catch (Exception e) {
+			System.out.println("SecurityFilterChain: " + e);
+		}
 
 		return http.build();
 	}
-	
 	
 	//Security for backend users
 	@Bean
@@ -68,5 +74,18 @@ public class WebSecurityConfig {
 		return new InMemoryUserDetailsManager(user);
 		
 	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin(address); // Allow frontend origin
+		configuration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+		configuration.addAllowedHeader("*"); // Allow all headers
+		configuration.setAllowCredentials(true); // If credentials like cookies are needed
 
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/**", configuration); // Apply CORS settings to all endpoints
+
+		return source;
+	}
 }
