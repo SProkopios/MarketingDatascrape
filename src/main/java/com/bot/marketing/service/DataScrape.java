a8fad76a558dffd7a18c8d09d626652a13c95177
Samu <119333647+SProkopios@users.noreply.github.com>
2025-05-29 20:29:47 +0300

    Update DataScrape.java

--- src/main/java/com/bot/marketing/service/DataScrape.java
+++ src/main/java/com/bot/marketing/service/DataScrape.java
@@ -93,6 +93,7 @@ public class DataScrape {
 			for (JsonNode e : results) {
 
 				System.out.println("email present: " + e.get(emailIsPresent));
+				System.out.println(emailIsPresent);
 				Boolean emailPresent = e.get(emailIsPresent).asBoolean();
 
 				if (emailPresent && company.getEmail().isEmpty()) {
