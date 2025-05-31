b257793c7ccf607138f9f4e94d0742b6d08ca769
Samu <119333647+SProkopios@users.noreply.github.com>
2025-05-31 11:08:23 +0300

    Update DataScrape.java

--- src/main/java/com/bot/marketing/service/DataScrape.java
+++ src/main/java/com/bot/marketing/service/DataScrape.java
@@ -29,9 +29,6 @@ public class DataScrape {
 	private static String emailElement = System.getenv("EmailElement");
 	private static String dataSource = System.getenv("SecondData");
 	
-
-	//Scrapes data using companys information
-	//calls isValidCompany-function
 	public static Company Scrape(Company company) {
 			try {
 				ObjectMapper mapper = new ObjectMapper();
@@ -43,36 +40,30 @@ public class DataScrape {
 					
 				String searchword = URLEncoder.encode(word, StandardCharsets.UTF_8.toString());					
 				String finalUrl = Url + searchword + UrlEnd;
-				System.out.println("final: " + finalUrl);
 				
 				Document doc = Jsoup.connect(finalUrl)
-						.timeout(4000)
-						.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-						.referrer("http://www.google.com")
-					    .header("Accept-Language", "fi-FI,en;q=0.9")
-					    .header("Accept-Encoding", "gzip, deflate, br")
-					    .header("Connection", "close")
-						.followRedirects(true)
-						.get();
+					.timeout(4000)
+					.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+					.referrer("http://www.google.com")
+					.header("Accept-Language", "fi-FI,en;q=0.9")
+					.header("Accept-Encoding", "gzip, deflate, br")
+					.header("Connection", "close")
+					.followRedirects(true)
+					.get();
 				
 				if(doc.childNodes().size() == 2) {
-
 					company.setSource(company.getSource() + ", " + dataSource);
 					Element element = doc.select(firstElement).first();
 					String jsonData = element.data();
-
-					System.out.println("jsonData: " + jsonData);
 					
 					if (!jsonData.isEmpty()) {
 						JsonNode node = mapper.readTree(jsonData);
 						JsonNode data = node.at(jsonPath);
-						
 						int otherSleepyTime = ThreadLocalRandom.current().nextInt(6000, 14001);
-						Thread.sleep(otherSleepyTime);
 						
+						Thread.sleep(otherSleepyTime);
 						company.setLink(finalUrl);
 						company = isValidCompany(data, company);
-						
 					} else {
 						System.out.println("No element found in Scrape!");
 					}
@@ -85,20 +76,10 @@ public class DataScrape {
 			return company;
 		}
 	
-	
-	//Validating the data further from Scrape
-	//If valid, return very informative company else return less informative
 	public static Company isValidCompany(JsonNode results, Company company) {	
 		try {
 			for (JsonNode e : results) {
-
-				System.out.println("e: " + e);
-
-				System.out.println("email present: " + e.get(emailIsPresent));
-				System.out.println("address is present: " + e.get("hasAddress"));
-				System.out.println(emailIsPresent);
 				Boolean emailPresent = e.get(emailIsPresent).asBoolean();
-
 				if (company.getEmail().isEmpty()) {
 					String companyId = e.get(idElement).asText();
 					companyId = refactorString(companyId);
@@ -109,7 +90,6 @@ public class DataScrape {
 						String email  = e.get(emailElement).asText();
 						company.setEmail(email);
 						break;
-						
 					} else {
 						company.setLink(null);
 					}
@@ -123,9 +103,6 @@ public class DataScrape {
 		return company;
 	}
 		
-	
-	
-	//Verify what name of the company is the correct one
 	public static Company getCorrectName(String Name, Company company) {
 		List<String> companyNames = company.getName();
 		for (String c : companyNames) {
@@ -139,13 +116,10 @@ public class DataScrape {
 		return company;
 	}
 	
-	
 	public static String refactorString(String string) {
 	    StringBuilder factoredString = new StringBuilder(string); 
 	    factoredString.insert(factoredString.length() - 1, "-"); 
 	    String result = factoredString.toString();
 	    return result;
 	}
-
-	
 }
